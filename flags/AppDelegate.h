abf2bde48d95f25053d55d56292a8330580384d8
Chris Patuzzo <chris@patuzzo.co.uk>
2014-03-22 11:49:58 +0000

    Initial commit

---
+++ flags/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  flags
+//
+//  Created by chris on 22/03/2014.
+//  Copyright (c) 2014 chris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ flags/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  flags
+//
+//  Created by chris on 22/03/2014.
+//  Copyright (c) 2014 chris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
