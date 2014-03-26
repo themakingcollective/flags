1915510676460ea9733e06990770e1c0435611f5
Chris Patuzzo <chris@patuzzo.co.uk>
2014-03-26 10:30:28 +0000

    Set up the TableController

---
+++ flags/TableController.h
@@ -0,0 +1,13 @@
+//
+//  TableController.h
+//  flags
+//
+//  Created by chris on 26/03/2014.
+//  Copyright (c) 2014 chris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableController : UITableViewController
+
+@end
---
+++ flags/TableController.m
@@ -0,0 +1,57 @@
+//
+//  TableController.m
+//  flags
+//
+//  Created by chris on 26/03/2014.
+//  Copyright (c) 2014 chris. All rights reserved.
+//
+
+#import "TableController.h"
+
+@interface TableController ()
+
+@end
+
+@implementation TableController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+
+@end
