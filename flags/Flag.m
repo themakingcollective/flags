6cf8441219041ca3295d8d7907c1180c3895cdb9
Chris Patuzzo <chris@patuzzo.co.uk>
2014-06-29 15:54:11 +0100

    Memoize +all and -metadata

--- flags/Flag.m
+++ flags/Flag.m
@@ -11,12 +11,14 @@
 @interface Flag ()
 
 @property (nonatomic, strong, readwrite) NSString *name;
+@property (nonatomic, strong) NSDictionary *metadataCache;
 
 @end
 
 @implementation Flag
 
 @synthesize name=_name;
+static NSArray *allCache;
 
 - (id)initWithName:(NSString *)name
 {
@@ -29,20 +31,28 @@
 
 - (NSDictionary *)metadata
 {
-    NSString *filename = [NSString stringWithFormat:@"%@/%@/metadata.json", [self.class directoryName], self.name];
-    NSData *json = [NSData dataWithContentsOfFile:filename options:NSDataReadingMappedIfSafe error:nil];
-    return [NSJSONSerialization JSONObjectWithData:json options:NSJSONReadingMutableContainers error:nil];
+    if (!self.metadataCache) {
+        NSString *filename = [NSString stringWithFormat:@"%@/%@/metadata.json", [self.class directoryName], self.name];
+        NSData *json = [NSData dataWithContentsOfFile:filename options:NSDataReadingMappedIfSafe error:nil];
+        self.metadataCache = [NSJSONSerialization JSONObjectWithData:json options:NSJSONReadingMutableContainers error:nil];
+    }
+    
+    return self.metadataCache;
 }
 
 + (NSArray *)all
 {
-    NSMutableArray *flags = [[NSMutableArray alloc] init];
-    
-    for (NSString *name in [self flagNames]) {
-        [flags addObject:[[Flag alloc] initWithName:name]];
+    if (!allCache) {
+        NSMutableArray *flags = [[NSMutableArray alloc] init];
+        
+        for (NSString *name in [self flagNames]) {
+            [flags addObject:[[Flag alloc] initWithName:name]];
+        }
+        
+        allCache = [NSArray arrayWithArray:flags];
     }
     
-    return [NSArray arrayWithArray:flags];
+    return allCache;
 }
 
 + (NSArray *)flagNames
