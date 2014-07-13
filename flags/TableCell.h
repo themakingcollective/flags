076ce24d660857671d504c76e10e6a2c4a3d5a78
Chris Patuzzo <chris@patuzzo.co.uk>
2014-07-13 17:10:03 +0100

    Create a custom table cell

---
+++ flags/TableCell.h
@@ -0,0 +1,13 @@
+//
+//  TableCell.h
+//  flags
+//
+//  Created by Chris Patuzzo on 13/07/2014.
+//  Copyright (c) 2014 chris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableCell : UITableViewCell
+
+@end
---
+++ flags/TableCell.m
@@ -0,0 +1,20 @@
+//
+//  TableCell.m
+//  flags
+//
+//  Created by Chris Patuzzo on 13/07/2014.
+//  Copyright (c) 2014 chris. All rights reserved.
+//
+
+#import "TableCell.h"
+
+@implementation TableCell
+
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    
+    // todo
+}
+
+@end
