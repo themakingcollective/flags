fb265451a4593dfd31e636825f5dda3771c0aa81
Chris Patuzzo <chris@patuzzo.co.uk>
2014-08-03 16:18:32 +0100

    Functional image-to-name quiz game

--- flags/ImageToNameController.m
+++ flags/ImageToNameController.m
@@ -11,6 +11,8 @@
 #import "DifficultyScaler.h"
 #import "Flag.h"
 #import "ScoringService.h"
+#import "HighlightsController.h"
+#import "EventRecorder.h"
 
 @interface ImageToNameController ()
 
@@ -36,12 +38,12 @@
     self.quiz = [[Quiz alloc] initWithArray:flags andRounds:10];
     
     [[ScoringService sharedInstance] reset];
-    [self nextFlag];
+    [self nextFlag:nil];
     
     [super viewDidLoad];
 }
 
-- (void)nextFlag
+- (void)nextFlag:(NSTimer *)timer
 {
     Flag *flag = [self.quiz currentElement];
     
@@ -51,23 +53,29 @@
         [self updateButtons];
     }
     else {
-        NSLog(@"finished!");
+        [self highlights];
     }
 }
 
 - (void)updateButtons
 {
+    UIColor *blue = [UIColor colorWithRed:(110 / 255.0) green:(149 / 255.0) blue:(233 / 255.0) alpha:1.0f];
+    
     Flag *aFlag = [self.choices objectAtIndex:0];
     [self.aButton setTitle:[aFlag name] forState:UIControlStateNormal];
+    [self.aButton setBackgroundColor:blue];
     
     Flag *bFlag = [self.choices objectAtIndex:1];
     [self.bButton setTitle:[bFlag name] forState:UIControlStateNormal];
+    [self.bButton setBackgroundColor:blue];
     
     Flag *cFlag = [self.choices objectAtIndex:2];
     [self.cButton setTitle:[cFlag name] forState:UIControlStateNormal];
+    [self.cButton setBackgroundColor:blue];
     
     Flag *dFlag = [self.choices objectAtIndex:3];
     [self.dButton setTitle:[dFlag name] forState:UIControlStateNormal];
+    [self.dButton setBackgroundColor:blue];
 }
 
 - (IBAction)submit:(UIButton *)button
@@ -76,14 +84,39 @@
     Flag *flag = [Flag find_by_name:flagName];
     
     if ([flag isEqualTo:[self.quiz currentElement]]) {
-//        [[ScoringService sharedInstance] correctForFlag:flag andMode:@"quiz" andDifficulty:@"image_to_name"];
+        [self recordEvent:YES flag:flag];
+        [[ScoringService sharedInstance] correctForFlag:flag andMode:@"quiz" andVariant:@"image_to_name"];
+        [button setBackgroundColor:[UIColor greenColor]];
     }
     else {
-//        [[ScoringService sharedInstance] incorrectForFlag:flag andMode:@"quiz" andDifficulty:@"image_to_name"];
+        [self recordEvent:NO flag:flag];
+        [[ScoringService sharedInstance] incorrectForFlag:flag andMode:@"quiz" andVariant:@"image_to_name"];
+        [button setBackgroundColor:[UIColor redColor]];
     }
     
     [self.quiz nextRound];
-    [self nextFlag];
+    [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(nextFlag:) userInfo:nil repeats:NO];
+}
+
+- (void)highlights
+{
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle: nil];
+    HighlightsController *highlights = [storyboard instantiateViewControllerWithIdentifier:@"HighlightsController"];
+    
+    highlights.mode = @"quiz";
+    highlights.variant = @"image_to_name";
+    
+    [self.navigationController pushViewController:highlights animated:YES];
+}
+
+- (void)recordEvent:(BOOL)playerWasCorrect flag:(Flag *)flag
+{
+    [[EventRecorder sharedInstance] record:@{
+         @"flag_name": [flag name],
+         @"mode": @"quiz",
+         @"variant": @"image_to_name",
+         @"correct": playerWasCorrect ? @"true" : @"false"
+    }];
 }
 
 @end
